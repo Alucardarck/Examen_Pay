3afbbf0e405be2ab764bb42dbd6374b3a65c84d1
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-08-28 15:54:10 -0500

    Initial Commit

---
+++ Examen_Pay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Examen_Pay
+//
+//  Created by Martin Leon Preciado on 28/08/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

a594852ee7ddaed29f4695d1cced5e5120ae46a4
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-08-28 16:28:10 -0500

    Information related to paypal has been implemented

--- Examen_Pay/Start.m
+++ Examen_Pay/Start.m
@@ -9,11 +9,26 @@
 #import "Start.h"
 
 @interface Start ()
-
+@property (nonatomic, strong, readwrite) PayPalConfiguration *payPalConfiguration;
 @end
 
 @implementation Start
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        _payPalConfiguration = [[PayPalConfiguration alloc] init];
+        
+        // See PayPalConfiguration.h for details and default values.
+        // Should you wish to change any of the values, you can do so here.
+        // For example, if you wish to accept PayPal but not payment card payments, then add:
+        _payPalConfiguration.acceptCreditCards = NO;
+        // Or if you wish to have the user choose a Shipping Address from those already
+        // associated with the user's PayPal account, then add:
+        _payPalConfiguration.payPalShippingAddressOption = PayPalShippingAddressOptionPayPal;
+    }
+    return self;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
